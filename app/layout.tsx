6e9857f1b47d3a14e5b6b5f72dee969bb8109725
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2025-05-21 09:58:44 +0300

    snap pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
   return (
     <html lang="ar" dir="rtl">
       <head>
-        {/* TikTok Pixel Code */}
+        {/* TikTok Pixel */}
         <script
           id="tiktok-pixel"
           dangerouslySetInnerHTML={{
@@ -70,7 +70,7 @@ export default function RootLayout({
               w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
               ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
               ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq, ttq.methods[i]);
               ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
               ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
               ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};
@@ -81,6 +81,22 @@ export default function RootLayout({
               ttq.load("D0M45C3C77U8DMAGO9H0");
               ttq.page();
             }(window, document, 'ttq');`,
+          }}
+        />
+
+        {/* Snap Pixel */}
+        <script
+          id="snap-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+              {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+              a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+              r.src=n;var u=t.getElementsByTagName(s)[0];
+              u.parentNode.insertBefore(r,u);})(window,document,
+              'https://sc-static.net/scevent.min.js');
+
+              snaptr('init', 'd9aaaff3-e67a-4544-b2d4-8c66877bc4e6');
+              snaptr('track', 'PAGE_VIEW');`,
           }}
         />
       </head>
